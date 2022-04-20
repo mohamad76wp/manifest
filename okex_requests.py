2b9387ac68b0b663f76befaab1ffc31397714754
mohamad76wp <mohamad76wp@gmail.com>
2022-04-20 16:11:25 +0430

    make a perfect position with Tp and Sl

--- okex_requests.py
+++ okex_requests.py
@@ -1,3 +1,7 @@
+from pydoc import resolve
+from textwrap import indent
+from turtle import pos
+from urllib import response
 import requests
 import base64
 import json
@@ -14,12 +18,29 @@ flag = "1"  #demo (1), real(0)
 
 tradeAPI = Trade.TradeAPI(api, secret, password, False, flag)
 
-place_order = tradeAPI.place_order(instId="BTC-USDT", tdMode='cross', side='buy',ordType='limit', sz='0.005', px='2000.00')
-get_position = tradeAPI.get
+instrument = "BTC-USDT-SWAP"
 
-print(place_order)
+import timeit
+start = timeit.default_timer()
+
+get_inst_price = requests.get("http://www.okex.com/api/v5/market/ticker?instId="+instrument)
+respon_lp = get_inst_price.json()["data"][0]["last"]
+
+print(respon_lp)
 print("\n")
+lp_int_tp = float(respon_lp) + 80
+lp_int_sl = float(respon_lp) - 80
 
+place_pos = tradeAPI.place_order(instId=instrument, tdMode='isolated',posSide="long" ,side='buy',ordType='market', sz=1)
+print(place_pos)
 
+# get_pos_list = tradeAPI.get_orders(instrument,place_pos["data"][0]["ordId"])
+# print(get_pos_list)
 
-    
+place_algo_order = tradeAPI.place_algo_order(instId=instrument, tdMode="isolated", side="sell",posSide="long", ordType="oco", sz="1" , tpTriggerPx = lp_int_tp  ,tpOrdPx = "-1", slTriggerPx = lp_int_sl  ,slOrdPx = "-1")
+print(place_algo_order)
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
+
