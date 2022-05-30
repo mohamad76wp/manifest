1e2ca8396ef5e6763e92e23c1498d693a9d0c0bf
mohamad76wp <mohamad76wp@gmail.com>
2022-05-31 00:03:22 +0430

    change the algo order [ordType] for using trailing stop algorithm

--- okex_requests.py
+++ okex_requests.py
@@ -1,9 +1,7 @@
-import re
-from urllib import response
-import requests
 import okexApi.okex.Trade_api as Trade
+import sys
+# import okx.Trade_api as Trade
 import secrets
-import time
 
 api = secrets.api()
 secret = secrets.secret()
@@ -23,29 +21,54 @@ start = timeit.default_timer()
 
 # print(respon_lp)
 
+def tagMaker(Secure_TP,Max_tp):
+    if len(Secure_TP) == 7:
+        Secure_TP = f"{Secure_TP}0" 
+    elif len(Secure_TP) < 7:
+        Secure_TP = f"{Secure_TP}.00"
+    scTP = Secure_TP.replace(".","x")
+
+
+    if len(Max_tp) == 7:
+        Max_tp = f"{Max_tp}0"
+    elif len(Max_tp) < 7:
+        Max_tp = f"{Max_tp}.00" 
+    mTP = Max_tp.replace(".","x")
+
+    tag = f"{scTP}{mTP}"
+    return tag
+
 
 class Manifest_oco:
     print("log 1")
     def __init__(self,api, secret, password, flag) :
         self.tradeAPI = Trade.TradeAPI(api, secret, password, False, flag)
 
-    def pos_long(self, instrument, Secure_TP, StopLoss):
+    def pos_long(self, instrument, Secure_TP, StopLoss, Max_tp):
+
+        tag = tagMaker(Secure_TP,Max_tp)
+        print(tag)
+
         place_pos = self.tradeAPI.place_order(instId=instrument, tdMode="isolated", posSide="long", side="buy", ordType='market', sz=3 )
-        place_algo_order = tradeAPI.place_algo_order(instId=instrument, tdMode="isolated", side="sell", posSide="long", ordType="oco", sz="3", tpTriggerPx = Secure_TP, tpOrdPx = "-1", slTriggerPx = StopLoss, slOrdPx = "-1")
+        place_algo_order = tradeAPI.place_algo_order(instId=instrument, tdMode="isolated", side="sell", posSide="long", ordType="conditional", sz="3", slTriggerPx = StopLoss, slOrdPx = "-1", tag=tag)
         return {"place_pos":place_pos,"place_algo_order":place_algo_order}
         
-    def pos_short(self, instrument, Secure_TP, StopLoss):
+    def pos_short(self, instrument, Secure_TP, StopLoss, Max_tp):
+
+        tag = tagMaker(Secure_TP,Max_tp)
+        print(tag)
+
         place_pos = self.tradeAPI.place_order(instId=instrument, tdMode="isolated", posSide="short", side="sell", ordType='market', sz=3 )
-        place_algo_order = tradeAPI.place_algo_order(instId=instrument, tdMode="isolated", side="buy", posSide="short", ordType="oco", sz="3", tpTriggerPx = Secure_TP, tpOrdPx = "-1", slTriggerPx = StopLoss, slOrdPx = "-1")
+        place_algo_order = tradeAPI.place_algo_order(instId=instrument, tdMode="isolated", side="buy", posSide="short", ordType="conditional", sz="3", slTriggerPx = StopLoss, slOrdPx = "-1", tag=tag)
         return {"place_pos":place_pos,"place_algo_order":place_algo_order}
 
 make_oco_trade = Manifest_oco(api,secret,password,flag)
 
-def send_oco_pos(side, size,secure_tp,stop_loss):
+def send_oco_pos(side, size,secure_tp,stop_loss,max_tp):
 
     if side == "long":
-        open_long_pos = make_oco_trade.pos_long("BTC-USDT-SWAP", secure_tp, stop_loss)
+        open_long_pos = make_oco_trade.pos_long("BTC-USDT-SWAP", secure_tp, stop_loss, max_tp)
         return open_long_pos
     elif side == "short":
-        open_short_pos = make_oco_trade.pos_short("BTC-USDT-SWAP", secure_tp, stop_loss)
+        open_short_pos = make_oco_trade.pos_short("BTC-USDT-SWAP", secure_tp, stop_loss, max_tp)
         return open_short_pos
